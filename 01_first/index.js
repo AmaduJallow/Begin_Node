d8e4519b8f93c19d71f5788e3bd3e58037cc460c
Amadu Jallow <aj22011021@utg.edu.gm>
2022-11-05 00:27:14 +0000

    This is the super commit

---
+++ 01_first/index.js
@@ -0,0 +1,8 @@
+require('./logger');
+const {log} = require("./logger");
+let sayHello = function(message){
+    return `This is your message: ${message}`;
+};
+let result = sayHello("This is your work right?");
+
+log(result);
