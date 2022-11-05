a94a6ac1b15f5e20b29bd620b614c237f031f15f
Amadu Jallow <aj22011021@utg.edu.gm>
2022-11-05 00:54:02 +0000

    This is me and Node.js

--- 01_first/fs.js
+++ 01_first/fs.js
@@ -1,2 +1,11 @@
 const fs = require('fs');
-console.log(fs.readdirSync("./"))
+// console.log(fs.readdirSync("./"));
+
+fs.readdir("./",function(err,filename){
+    if (!err){
+        console.log(filename);
+    }
+    else{
+        console.log(err);
+    }
+})
